c9a394a856c44fb5b9fdc7a41c035f6b68dc1acf
Claudio Florentin <clauramon2000@gmail.com>
2018-07-30 22:37:43 -0300

    controlador y vista compramed

---
+++ controllers/cliente.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# try something like
+def compramed(): 
+    return dict(message="hello from cliente.py")
---
+++ controllers/listaprove.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# try something like
+def proveedores(): 
+    return dict(message="hello from listaprove.py")
